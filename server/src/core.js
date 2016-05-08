fb4d76f322155b7249167c1b34d99b1e5156aec0
alisazzz <alisa.zwy@gmail.com>
2016-05-07 22:26:29 -0400

    Refactored vote using reducer composition.

--- server/src/core.js
+++ server/src/core.js
@@ -32,9 +32,9 @@ export const next = state => {
   }
 }
 
-export const vote = (state, entry) => {
-  return state.updateIn(
-    ["vote", "tally", entry],
+export const vote = (voteState, entry) => {
+  return voteState.updateIn(
+    ["tally", entry],
     0,
     tally => tally + 1
   )
